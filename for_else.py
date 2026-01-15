e9a038405ec64037c8a6f3f378b3bb1e303ed658
Vikas <Vikas.singh@learning.com>
2026-01-15 13:12:47 +0000

    For loop and while

---
+++ for_else.py
@@ -0,0 +1,6 @@
+#For else is used when code need to be executed after loop ended
+
+for i in range(8):
+    print(i)
+else:
+    print("For loop completed")
--- loops_learn.py
+++ loops_learn.py
@@ -7,6 +7,11 @@ for i in name:
 print("for loop ended")
 
 """
+
+for i in range(7,50,7):
+    print(i)
+
+
 #while loop
 
 # i=0
@@ -23,11 +28,11 @@ print("for loop ended")
 #     print(i)
 
 #while loop with continue
-i=0
-while (i<10):
-    i=i+1
-    if (i==5):
-     print ("skipping number 5")
-     continue
-    print(i*2)
+# i=0
+# while (i<10):
+#     i=i+1
+#     if (i==5):
+#      print ("skipping number 5")
+#      continue
+#     print(i*2)
     
