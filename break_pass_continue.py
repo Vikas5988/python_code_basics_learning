607d0f97241ffda9ccb78877fa1982bff449b7db
Vikas <Vikas.singh@learning.com>
2026-01-16 07:29:53 +0000

    break and pass

---
+++ break_pass_continue.py
@@ -0,0 +1,22 @@
+# Break Pass and Continue Statement
+
+#Break Statement
+
+for i in range(6):
+    if (i==3):
+        print("Break at  number 3")
+        break
+    print(i)
+    
+#Break Statement
+
+for i in range(6):
+    if (i==4):
+        print("Skip at 4 number")
+        continue
+    print(i)
+
+#Pass
+for i in range(6):
+      pass  # To Skip the error  and Do Nothing
+        
