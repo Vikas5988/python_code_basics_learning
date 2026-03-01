f4ec61e7ece2bb3a2df53d16ef0af18a22682cdf
Vikas <Vikas.singh@learning.com>
2026-03-01 15:48:13 +0000

    boto s3 file addition

---
+++ boto_s3.py
@@ -0,0 +1,35 @@
+import boto3
+
+
+s3=boto3.resource("s3")
+print(s3)
+def show_buckets(x):
+    for bucket in x.buckets.all():
+        print(bucket.name)
+               
+               
+def create_buckets(y):
+   # s3.create_bucket(Bucket="vsr-boto3-test-1-mar-2026")
+        s3.create_bucket(Bucket="vsr-boto3-test2-1-mar-2026",
+                     CreateBucketConfiguration={
+                         'LocationConstraint': 'ap-south-1', #it should match the default region set
+                         },)
+               
+show_buckets(s3)
+
+create_buckets(s3)
+
+show_buckets(s3)
+
+
+
+# s4 is written instead of S3 to check the function of boto3. 
+
+""" s5=boto3.resource("s3")
+def show_buckets(x):
+    for bucket in x.buckets.all():
+        print(bucket.name)
+               
+show_buckets(s5) """
+
+
